e34db344571c8b7a56000c0acc0a072896fda531
Viking & Robot <vikingandrobot@gmail.com>
2018-01-11 10:57:49 +0100

    Before pull

---
+++ src/main/java/ch/heigvd/prr_labo03/Applicatif.java
@@ -0,0 +1,9 @@
+package ch.heigvd.prr_labo03;
+
+/**
+ * Cette classe se charge de tester périodiquement que l'élu est vivant et de lancer
+ * une exception si ce n'est pas le cas
+ */
+public class Applicatif {
+
+}
