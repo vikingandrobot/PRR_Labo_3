5d94d18f1ff8170215276cd67f4e4f8ad8179168
Viking & Robot <vikingandrobot@gmail.com>
2018-01-11 10:50:59 +0100

    Refactored reading file.

--- src/main/java/ch/heigvd/prr_labo03/Process.java
+++ src/main/java/ch/heigvd/prr_labo03/Process.java
@@ -15,16 +15,13 @@ import javafx.util.Pair;
  *
  */
 public class Process {
-
-   public static void main(String[] args) throws InterruptedException {
-
-      String fileName = "/processes.txt";
-
-      // Lecture du fichier
+   
+   private static List<Pair<InetAddress, Integer>> readProcessesAddresses(String filename) 
+   throws Exception {
       try (
               BufferedReader buffer = new BufferedReader(
                       new InputStreamReader(
-                              Process.class.getResourceAsStream(fileName)
+                              Process.class.getResourceAsStream(filename)
                       )
               )) {
 
@@ -49,6 +46,39 @@ public class Process {
          
          processes.forEach(System.out::println);
          
+         return processes;
+
+         
+      } catch (IOException e) {
+         throw new Exception("Une erreur est survenue en lisant les fichiers.", e);
+      }
+   }
+
+   public static void main(String[] args) throws InterruptedException {
+      
+      int id = -1;
+      
+      if (args.length != 1) {
+         System.out.println("Le programme doit recevoir en paramètre son ID de site"
+                 + " (0 à 3).");
+         System.out.println("Lance le programme : java -jar <nom_application_jar> <id_du_site>");
+         //System.exit(1);
+         id = 0;
+      }
+      
+      if (id < 0)
+         id = Integer.parseInt(args[0]);
+      
+      if (id < 0 || id > 3) {
+         System.out.println("Erreur: l'ID de site doit être dans l'intervalle doit être "
+                 + "entre 0 et 3 compris.");
+         System.exit(1);
+      }
+
+      String fileName = "/processes.txt";
+      
+      try {
+         List<Pair<InetAddress, Integer>> processes = readProcessesAddresses(fileName);
          
          Election e1 = new Election(0, processes);
          Election e2 = new Election(1, processes);
@@ -58,14 +88,11 @@ public class Process {
          e1.startElection();
 
          Thread.sleep(1000);
-
-
-
-
          
-      } catch (IOException e) {
+      } catch (Exception ex) {
+         
          Logger.getLogger(Process.class.getName())
-                 .log(Level.SEVERE, null, e);
+                 .log(Level.SEVERE, null, ex);
       }
       
       System.exit(0);
